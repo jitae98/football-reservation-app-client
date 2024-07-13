38db654b47b0a74fa7c974e8eec5bbd0d11c338b
chua-dao <jitae98tu@naver.com>
2024-07-13 22:27:54 +0700

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import FormContainer from "./components/FormContainer.js";
 import ToggleContainer from "./components/ToggleContainer.js";
+import Main from "./components/Main.js";
 import "./App.css";
 
 const App = () => {
   const [isSignUpActive, setIsSignUpActive] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const handleSignInClick = () => {
     setIsSignUpActive(false);
@@ -14,18 +22,56 @@ const App = () => {
     setIsSignUpActive(true);
   };
 
+  const handleLoginSuccess = () => {
+    setIsAuthenticated(true);
+  };
+
   return (
-    <div
-      className={`container ${isSignUpActive ? "active" : ""}`}
-      id="container"
-    >
-      <FormContainer formType="sign-up" />
-      <FormContainer formType="sign-in" />
-      <ToggleContainer
-        onSignInClick={handleSignInClick}
-        onSignUpClick={handleSignUpClick}
-      />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <div
+              className={`container ${isSignUpActive ? "active" : ""}`}
+              id="container"
+            >
+              <FormContainer
+                formType="sign-in"
+                handleLoginSuccess={handleLoginSuccess}
+              />
+              <ToggleContainer
+                onSignInClick={handleSignInClick}
+                onSignUpClick={handleSignUpClick}
+              />
+            </div>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <div
+              className={`container ${isSignUpActive ? "active" : ""}`}
+              id="container"
+            >
+              <FormContainer
+                formType="sign-up"
+                handleLoginSuccess={handleLoginSuccess}
+              />
+              <ToggleContainer
+                onSignInClick={handleSignInClick}
+                onSignUpClick={handleSignUpClick}
+              />
+            </div>
+          }
+        />
+        <Route
+          path="/main"
+          element={isAuthenticated ? <Main /> : <Navigate to="/login" />}
+        />
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
   );
 };
 
