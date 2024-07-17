af42ec513074d7ffa4257cf3b495229288257704
qanhpeo17 <154793506+qanhpeo17@users.noreply.github.com>
2024-07-17 07:29:36 +0700

    qanh fix content.jsx

--- src/components/MainPage/Content/Content.jsx
+++ src/components/MainPage/Content/Content.jsx
@@ -1,38 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Content.css";
 
 function Content() {
+  const [fields, setFields] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/fields");
+        const data = await response.json();
+        setFields(data);
+      } catch (error) {
+        console.error("Error fetching fields:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
       <div className="contentWrapper">
         <div className="fieldtypeList">
-          <div className="field field1">
-            <p>Sân 5</p>
-            <p>
-              <strong>4/5</strong> sân đã thuê
-            </p>
-            <a href="/booking" className="orderBtn">
-              Đặt sân
-            </a>
-          </div>
-          <div className="field field2">
-            <p>Sân 7</p>
-            <p>
-              <strong>2/2</strong> sân đã thuê
-            </p>
-            <a href="/booking" className="orderBtn">
-              Đặt sân
-            </a>
-          </div>
-          <div className="field field3">
-            <p>Sân 11</p>
-            <p>
-              <strong>1/2</strong> sân đã thuê
-            </p>
-            <a href="/booking" className="orderBtn">
-              Đặt sân
-            </a>
-          </div>
+          {fields.map((field) => (
+            <div key={field.ID} className={`field field${field.ID}`}>
+              <p>{field.name}</p>
+              <p>
+                <strong>
+                  {field.bookedQuantity}/{field.quantity}
+                </strong>{" "}
+                sân đã thuê
+              </p>
+              <a href="/booking" className="orderBtn">
+                Đặt sân
+              </a>
+            </div>
+          ))}
         </div>
       </div>
     </div>
